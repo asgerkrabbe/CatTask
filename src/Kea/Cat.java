751af9fbf764ca4be8bc840edfddf629803d678a
asgerkrabbe <asgerkrabbe@gmail.com>
2020-10-29 10:44:14 +0100

    Initial commit

---
+++ src/Kea/Cat.java
@@ -0,0 +1,31 @@
+package Kea;
+
+public class Cat {
+
+    String name;
+    String sex;
+    int age;
+    int weight;
+    String color;
+    String texture;
+
+    public void catTemp(String name, String sex, int age, int weight, String color, String texture){
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+        this.texture = texture;
+    }
+
+    @Override
+    public String toString() {
+        return  "Name = " + name +
+                "\nSex = " + sex +
+                "\nAge = " + age +
+                "\nWeight = " + weight +
+                "\nColor = " + color +
+                "\nTexture = " + texture + "\n";
+
+    }
+}
---
+++ src/Kea/Main.java
@@ -0,0 +1,14 @@
+package Kea;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Cat oscar = new Cat();
+        Cat luna = new Cat();
+
+        oscar.catTemp("Oscar","Male",3,7,"Brown","Striped");
+        luna.catTemp("Luna","Female",2,5,"Gray","Plain");
+
+        System.out.println(luna + "" + oscar);
+    }
+}
